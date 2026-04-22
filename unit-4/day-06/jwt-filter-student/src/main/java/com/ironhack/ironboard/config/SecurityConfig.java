18dadd59fb866d77966974608f2fdbe781082a03
LloydC <lloydchambrier@gmail.com>
2026-04-22 10:15:42 +0000

    update unit 04 day 06

--- unit-4/day-06/jwt-filter-student/src/main/java/com/ironhack/ironboard/config/SecurityConfig.java
+++ unit-4/day-06/jwt-filter-student/src/main/java/com/ironhack/ironboard/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/auth/**").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated()
             )
             .exceptionHandling(ex ->
